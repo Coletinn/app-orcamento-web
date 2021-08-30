51b3158479fd18256408a32016b2b8a1fdc111a9
Coletinn <gustavocoleto@hotmail.com>
2021-08-30 18:28:31 -0300

    Validando dados antes do registro - parte 2

--- app.js
+++ app.js
@@ -63,9 +63,10 @@ function cadastrarDespesa() {
     )
 
     if (despesa.validarDados()) {
-        console.log('Dados válidos')
+        bd.gravar(despesa)
+        $('#sucessoGravacao').modal('show')
     } else {
-        console.log('Dados inválidos')
+        $('#erroGravacao').modal('show')
     }
 
 }
